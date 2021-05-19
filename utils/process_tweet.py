b996c618fb32c2ec0e71a36c9c81d1989d3fa143
sheshipal <sheshipalkarivedula@gmail.com>
2021-05-19 20:51:54 +0530

    added frequencies method

--- utils/process_tweet.py
+++ utils/process_tweet.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
@@ -19,48 +20,90 @@ punctuation = string.punctuation
 
 stemmer = PorterStemmer()
 
-def clean_tweet(tweet):
-    # cleaning the retweet words 'RT'
-    modified_tweet = re.sub(r'^RT[\s]+','',tweet)
+def clean_tweet(tweets):
+    stemmed_tweet_list = list()
+    for tweet in tweets:
+        # cleaning the retweet words 'RT'
+        modified_tweet = re.sub(r'^RT[\s]+','',tweet)
+        # cleaning hyperlinks
+        modified_tweet = re.sub(r'https?:\/\/.*[\r\n]*','',modified_tweet)
+        # removing '#' tags
+        modified_tweet = re.sub(r'#','',modified_tweet)
+        # tokenize the tweet
+        tokenized_tweet = tokenizer.tokenize(modified_tweet)
+        # removing stopwords and punctuations
+        clean_tweet = []
+        for token in tokenized_tweet:
+            if token not in stopwords_english and token not in punctuation:
+                clean_tweet.append(token)
+        # stemming the the word
+        stemmed_tweet = []
+        for word in clean_tweet:
+            stem_word = stemmer.stem(word)
+            stemmed_tweet.append(stem_word)
 
-    # cleaning hyperlinks
-    modified_tweet = re.sub(r'https?:\/\/.*[\r\n]*','',modified_tweet)
+        stemmed_tweet_list.append(stemmed_tweet)
+
+def create_frequencies(cleaned_positive_tweets, cleaned_negative_tweets):
+    frequencies = {}
+    for tweet in cleaned_positive_tweets:
+        for word in tweet:
+            if (word,1) in frequencies.keys:
+                frequencies[(word,1)] += 1
+            else:
+                frequencies[(word,1)] = 1
+
+    for tweet in cleaned_negative_tweets:
+        for (word,0) in tweet:
+            if word in frequencies.keys:
+                frequencies[(word,0)] += 1
+            else:
+                frequencies[(word,0)] = 1
     
-    # removing '#' tags
-    modified_tweet = re.sub(r'#','',modified_tweet)
+    return frequencies
 
-    # tokenize the tweet
-    tokenized_tweet = tokenizer.tokenize(modified_tweet)
+def create_vectors(frequencies, cleaned_positive_tweets, cleaned_negative_tweets):
+    m = len(cleaned_positive_tweets) + len(cleaned_negative_tweets)
+    data = []
+    for tweet in cleaned_positive_tweets:
+        positive_count = 0
+        negative_count = 0
+        for word in tweet:
+            positive_count += frequencies[(word,1)]
+            negative_count += frequencies[(word,0)]
+        data.append([1,positive_count,negative_count,1])
+    
+    for tweet in cleaned_negative_tweets:
+        positive_count = 0
+        negative_count = 0
+        for word in tweet:
+            positive_count += frequencies[(word,1)]
+            negative_count += frequencies[(word,0)]
+        data.append([1,positive_count,negative_count,0])
 
-    # removing stopwords and punctuations
-    clean_tweet = []
-    for token in tokenized_tweet:
-        if token not in stopwords_english and token not in punctuation:
-            clean_tweet.append(token)
-
-    # stemming the the word
-    stemmed_tweet = []
-    for word in clean_tweet:
-        stem_word = stemmer.stem(word)
-        stemmed_tweet.append(stem_word)
-
-    return stemmed_tweet
+    return data
 
 if __name__ == "__main__":
     all_positive_tweets = twitter_samples.strings("positive_tweets.json")
     all_negative_tweets = twitter_samples.strings("negative_tweets.json")
 
-    cleaned_positive_tweets = []
-    for tweet in all_positive_tweets:
-        cleaned_tweet = clean_tweet(tweet)
-        cleaned_positive_tweets.append(cleaned_tweet)
+    cleaned_positive_tweets = clean_tweet(all_positive_tweets)
+    cleaned_negative_tweets = clean_tweet(all_negative_tweets)
 
-    cleaned_negative_tweets = []
-    for tweet in all_negative_tweets:
-        cleaned_tweet = clean_tweet(tweet)
-        cleaned_negative_tweets.append(cleaned_tweet)
-    
+    frequencies = create_frequencies(cleaned_positive_tweets, cleaned_negative_tweets)
+
+    data = create_vectors(frequencies, cleaned_positive_tweets, cleaned_negative_tweets)
+
+     
     print("length of cleaned positive tweets : ",len(cleaned_positive_tweets))
     print("length of cleaned negative tweets : ",len(cleaned_negative_tweets))
     print("a positive tweet : ",cleaned_positive_tweets[381])
     print("a negative tweet : ",cleaned_negative_tweets[381])
+
+
+
+
+def extract_feature(stemmed_tweet):
+    for word in stemmed_tweet:
+        pass
+    pass
